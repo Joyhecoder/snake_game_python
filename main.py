81989a2bc52f2841be03c743feb9d5b5a1687fbf
Your GitHub Username <joyhe520@gmail.com>
2024-02-26 11:40:57 -0600

    refactor main.py and brought in snake class

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from turtle import Screen, Turtle
 import time 
+from snake import Snake
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -7,24 +8,16 @@ screen.bgcolor("black")
 screen.title("Snake Game")
 screen.tracer(0)
 
-segments = []
-
-#set up 3-segment snake
-starting_positions = [0, -20, -40]
-for i in starting_positions:
-    turtle1 = Turtle("square")
-    turtle1.color('white')
-    turtle1.penup()
-    turtle1.setposition(x=i, y=0)
-    segments.append(turtle1)
+snake = Snake()
 
 
 game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-    for seg in segments:
-        seg.forward(20)
+    snake.move()
+    
+
         
 
 
