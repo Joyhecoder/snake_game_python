3eadda1997194cf7930dab81e7bc3ef4cc0e74f9
Your GitHub Username <joyhe520@gmail.com>
2024-02-26 10:06:17 -0600

    set up 3 segments

--- main.py
+++ main.py
@@ -5,6 +5,12 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("Snake Game")
 
+#set up 3-segment snake
+starting_positions = [0, -20, -40]
+for i in starting_positions:
+    turtle1 = Turtle("square")
+    turtle1.color('white')
+    turtle1.setposition(x=i, y=0)
 
 
 
