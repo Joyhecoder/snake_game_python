31f295cb79eee43119f34e363d1391c12ff89fc3
Your GitHub Username <joyhe520@gmail.com>
2024-02-27 10:54:18 -0600

    updated food and scoreboard class

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from turtle import Screen, Turtle
+from turtle import Screen
 import time 
 from snake import Snake
+from scoreboard import Scoreboard
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -9,6 +11,8 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 snake = Snake()
+food = Food()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -21,6 +25,11 @@ while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+    
+    #Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
+        scoreboard.get_a_point()
     
 
         
